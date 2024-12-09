875ba0827c2132e8be722178f270b202a62ddd58
Christopher Makousky <christopher.makousky@gmail.com>
2024-12-09 05:56:23 -0600

    completed Nav component and Candidate interface

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,7 +1,22 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import type Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
+  const [] = useState<Candidate>({
+    name: '',
+    username: '',
+    location: '',
+    avatar: '',
+    email: '',
+    html_url: '',
+    company: '',
+  });
+
+  const [searchInput, setSearchInput] = useState<string>('');
+
+  
+
   return <h1>CandidateSearch</h1>;
 };
 
